8705e93e516afc34c3f92601e44d99e1fc409341
graykode <nlkey2022@gmail.com>
2019-11-07 21:59:44 +0900

    change print console to file write

--- sink.py
+++ sink.py
@@ -18,18 +18,22 @@ def main(args):
     sock = ctx.socket(zmq.PULL)
     sock.bind("tcp://*:%d" % (args.sport))
 
-    print("STARTING SERVER AT %d" % (args.sport))
+    f = open("logger", 'w')
+
+    f.write("STARTING SERVER AT %d\n" % (args.sport))
 
     sig_start = sock.recv()
     v = int.from_bytes(sig_start, 'big')
-    print("GATHER %d TASKS." % (v))
+    f.write("GATHER %d TASKS.\n" % (v))
     cnt = 0
 
     for task_no in range(v):
         data = sock.recv_string()
         if data == 'DONE':
             cnt += 1
-            print("%d/%d(%.2f) percent TASKS WAS DONE" % (cnt, v, (cnt / v) * 100))
+            f.write("%d/%d(%.2f) percent TASKS WAS DONE\n" % (cnt, v, (cnt / v) * 100))
+
+    f.close()
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='sink for masked '
