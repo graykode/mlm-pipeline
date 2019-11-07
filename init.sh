95d9f987e9f89a9b6d9468c5c3f3b70b00a5dac5
graykode <nlkey2022@gmail.com>
2019-11-07 21:59:25 +0900

    change init shell to install terraform and ansible

--- init.sh
+++ init.sh
@@ -1,5 +1,15 @@
-# init shell for ventilator and sink
+# install terraform and ansible
 sudo apt update
-sudo apt install -y python3
-sudo apt install -y python3-pip
-pip3 install zmq
+
+sudo apt-get install python3 python-dev python3-dev \
+     build-essential libssl-dev libffi-dev \
+     libxml2-dev libxslt1-dev zlib1g-dev \
+     python-pip -y
+
+wget https://releases.hashicorp.com/terraform/0.11.13/terraform_0.11.13_linux_amd64.zip && \
+    unzip terraform_0.11.13_linux_amd64.zip && \
+    rm terraform_0.11.13_linux_amd64.zip && \
+    sudo mv terraform /usr/bin && sudo chmod +x /usr/bin/terraform
+
+wget https://raw.githubusercontent.com/alicek106/aws-terraform-kubernetes/4c1e79e9ee6299afb846fee1e763d8ff9b9d88b0/ansible/requirements.txt && \
+    pip3 install -r requirements.txt
